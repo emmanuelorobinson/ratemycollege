6ff2349597227110b3fedb09a00193efb6e4395a
Emmanuel Robinson <emmanuelorobinson@github.com>
2022-08-14 11:55:48 +0400

    basic layout

---
+++ pages/index.jsx
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  const meta = {
+    title: "RateMyCollegeDubai",
+    description: `The #1 student focused site for college reviews`,
+    image: "/logo.png",
+    type: "website",
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="description" content={meta.description} />
+        <link rel="icon" href="/logo.png" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        ></meta>
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta property="author" content={meta.title} />
+      </Head>
+
+      <main>
+        <h1>RateMyCollege</h1>
+      </main>
+    </div>
+  );
+}
---
+++ pages/university/[university].jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const University = () => {
+  return (
+    <div>[university]</div>
+  )
+}
+
+export default University
